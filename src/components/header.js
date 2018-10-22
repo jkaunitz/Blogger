00a0c261cc726e569b6f41fa3d7442f466ca3125
Justin Kaunitz <justin.kaunitz@gmail.com>
2018-10-22 11:12:33 -0700

    refactored quite a bit.

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <div className='header'>
+            <h1 className='header__title'>"Publish the blogs that you want"</h1>
+            <p className='header__subtitle'>Please sign in to continue</p>
+        </div>
+    )
+}
+
+export function HeaderBar() {
+    return (
+        <div className='bar'></div>
+    )
+}
